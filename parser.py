d4a562a386eff5c582626e692681376382375723
Brendan Samek <xrendan@gmail.com>
2016-11-12 14:12:09 -0700

    Finds Course numbers and names

--- parser.py
+++ parser.py
@@ -11,4 +11,9 @@ import requests
 
 r = requests.get("https://catalogue.ualberta.ca/Course/Subject?subjectCode=ANTHR")
 soup = BeautifulSoup(r.text, "html.parser")
-print(soup.get_text())
+courses = soup.find_all("div", class_="claptrap-course")
+
+for i in courses:
+    print(i.find("span", class_="claptrap-course-number").string.strip())
+    print(i.find("span", class_="claptrap-course-title").string.strip())
+    print(i.find("p"))
