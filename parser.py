c0aafd289e2607d6abf4ca627dfc58f9d06edef1
Brendan Samek <xrendan@gmail.com>
2016-11-12 16:33:50 -0700

    Prints all course numbers, names and descriptions

--- parser.py
+++ parser.py
@@ -9,20 +9,55 @@ Created on Sat Nov 12 12:21:30 2016
 from bs4 import BeautifulSoup
 import requests
 
-r = requests.get("https://catalogue.ualberta.ca/Course/Subject?subjectCode=SCI")
-soup = BeautifulSoup(r.text, "html.parser")
-courses = soup.find_all("div", class_="claptrap-course")
 
-for i in courses:
+soup_Faculties = BeautifulSoup(requests.get("https://catalogue.ualberta.ca/Course").text, "html.parser")
+
+faculty_Links = soup_Faculties.find("table").find_all("a")
+
+facultyCodes = []
+facultyNames = []
+
+for link in faculty_Links:
+    facultyNames.append(link.string.strip())
+    facultyCodes.append(link.attrs["href"].replace("/Course/Faculty?facultyCode=", ""))
+
+
+for idx, faculty in enumerate(facultyCodes):
+    r = requests.get("https://catalogue.ualberta.ca/Course/Faculty", params = {"facultyCode": faculty})
+    soup_Subject = BeautifulSoup(r.text, "html.parser")
+    subject_Rows = soup_Subject.find("table").find_all("tr")
     
-    #course number
-    print(i.find("span", class_="claptrap-course-number").string.strip())
+    #print(facultyNames[idx])
     
-    #course name
-    print(i.find("span", class_="claptrap-course-title").string.strip())
-    
-    #Course Description
-    try:
-        print(i.find("p").contents[2].strip())
-    except:
-        print("No Description Available")
+    subjectNames = []
+    subjectCodes = []
+    for row in subject_Rows:
+        #subject_Cols = subject_Rows.find("td")
+        try:
+            subjectNames.append(row.find_all("td")[1].string.strip())
+            subjectCodes.append(row.find("a").attrs["href"].replace("/Course/Subject?subjectCode=", ""))
+        except:
+            pass
+    #print(subjectCodes)
+
+
+    for subject in subjectCodes:
+
+            
+        s = requests.get("https://catalogue.ualberta.ca/Course/Subject", params = {"subjectCode": subject})
+        soup = BeautifulSoup(s.text, "html.parser")
+        courses = soup.find_all("div", class_="claptrap-course")
+        
+        for i in courses:
+            
+            #course number
+            print(i.find("span", class_="claptrap-course-number").string.strip())
+            
+            #course name
+            print(i.find("span", class_="claptrap-course-title").string.strip())
+            
+            #Course Description
+            try:
+                print(i.find("p").contents[2].strip())
+            except:
+                print("No Description Available")
