ff31318763a52b215fcbe937c86c5beac4230342
Brendan Samek <xrendan@gmail.com>
2016-11-12 14:30:35 -0700

    No Course Description Error Handling

--- parser.py
+++ parser.py
@@ -9,11 +9,20 @@ Created on Sat Nov 12 12:21:30 2016
 from bs4 import BeautifulSoup
 import requests
 
-r = requests.get("https://catalogue.ualberta.ca/Course/Subject?subjectCode=ANTHR")
+r = requests.get("https://catalogue.ualberta.ca/Course/Subject?subjectCode=SCI")
 soup = BeautifulSoup(r.text, "html.parser")
 courses = soup.find_all("div", class_="claptrap-course")
 
 for i in courses:
+    
+    #course number
     print(i.find("span", class_="claptrap-course-number").string.strip())
+    
+    #course name
     print(i.find("span", class_="claptrap-course-title").string.strip())
-    print(i.find("p"))
+    
+    #Course Description
+    try:
+        print(i.find("p").contents[2].strip())
+    except:
+        print("No Description Available")
