aa64b68af213c4cdcecc231e0b591cb9f6aa4cf0
Brendan Samek <xrendan@gmail.com>
2016-11-12 13:09:58 -0700

    Created README and Parser.py
    Created Readme, created parser.py, parser.py currently gets one page
    from the course catalogue then prints the text from that webpage

---
+++ parser.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 12 12:21:30 2016
+
+@author: brendan
+"""
+
+from bs4 import BeautifulSoup
+import requests
+
+r = requests.get("https://catalogue.ualberta.ca/Course/Subject?subjectCode=ANTHR")
+soup = BeautifulSoup(r.text, 'html.parser')
+print(soup.get_text())
+input("")
---
+++ readme.md
@@ -0,0 +1,1 @@
+# Ualberta Prerequisite Explorer
