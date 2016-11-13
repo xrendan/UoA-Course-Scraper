d0a5d22ea01f3dee46e2c219b6e2de0c652c12ff
Brendan Samek <xrendan@gmail.com>
2016-11-13 11:59:22 -0700

    He's Dead Jim

--- gui.py
+++ gui.py
@@ -31,7 +31,76 @@ def toggle(i):
         
     else:
         l[i] = 1
-    print(l)
+    #print(l)
+    secondwindow = tkinter.Tk()
+    secondwindow.title("Plots")
+    secondwindow.geometry("600x800")
+    secondwindow.configure(background="SteelBlue1")
+    
+    w = tkinter.Frame(secondwindow)
+    x = tkinter.Frame(secondwindow)
+    w.pack()
+    x.pack()
+    
+    courses = []
+    for idx, j in enumerate(l):
+        if j == 1:
+            courses.append(n[idx])
+    
+            
+    classes = []
+    for idx, i in enumerate(courses):
+        lbl0 = tkinter.Label(w, text = i, bg="SteelBlue1", font=('Helvetica',20))
+        lbl0.pack()
+#        lbl0 = tkinter.Label(w, text = "|", bg="SteelBlue1", font=('Helvetica',20))
+#        lbl0.pack(side=tkinter.LEFT)
+        print()
+        print(i)
+        c.execute('SELECT CourseNum FROM programs WHERE Program = ?', (i,))
+        m = c.fetchall()
+        t = []
+        for p in m:
+            t.append(p[0])
+        classes.append(t)
+        
+        for i, val in enumerate(classes[idx]):
+            btn = tkinter.Button(w, text = val, command = lambda index=i: description(val))
+            btn.pack(side=tkinter.TOP)
+    
+    print(classes[0])
+    
+    secondwindow.mainloop()
+
+    
+def description(i):
+ 
+    thirdwindow = tkinter.Tk()
+    thirdwindow.title("Plots")
+    thirdwindow.geometry("1500x800")
+    thirdwindow.configure(background="green")
+    
+    lbl0 = tkinter.Label(thirdwindow, text = i, bg="SteelBlue1", font=('Helvetica',14))
+    lbl0.pack()      
+    
+
+    print()
+    print(i)
+    c.execute('SELECT CourseDescription FROM courses WHERE CourseNum = ?', (i,))
+    m = c.fetchall()
+    print(m)
+    t = []
+    jim = []
+    for p in m:
+        t.append(p[0])
+    jim.append(t)
+    
+    btn = tkinter.Label(thirdwindow, text = jim[0], font=('Helvetica',20))
+    btn.pack(side=tkinter.TOP)
+
+
+    
+    thirdwindow.mainloop()   
+    
     
 
 col = "indian red"
@@ -43,8 +112,9 @@ window.configure(background=col)
 lbl = tkinter.Label(window, text ="Engineering Program Comparer", bg=col, font=('Helvetica',30))
 lbl.pack()
 
+
 for i, val in enumerate(n):
     btn = tkinter.Button(window, text = val, command = lambda index=i: toggle(index))
     btn.pack(side=tkinter.TOP)
 # Code to add widgets will go here...
-r = window.mainloop()
+window.mainloop()
