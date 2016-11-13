df6886af23314931e261b20484f5cafbbbd89521
Brendan Samek <xrendan@gmail.com>
2016-11-12 20:48:52 -0700

    Did something with SQL, will it work? Who knows

--- create_db.py
+++ create_db.py
@@ -11,13 +11,19 @@ import sqlite3
    
 sqlite_file = 'course_listings.sqlite'    
 table_name = 'courses'  # name of the table to be created
-new_field = 'CourseNumber' # name of the column
+table_name1 = 'prerequisites'  # name of the table to be created
+table_name2 = 'conflicts'  # name of the table to be created
+table_name3 = 'corequisites'  # name of the table to be created
+new_field = 'CourseNum' # name of the column
 field_type = 'TEXT'  # column data type
 
 conn = sqlite3.connect(sqlite_file)
 c = conn.cursor()
 
 c.execute('CREATE TABLE {tn} ({nf} {ft})'.format(tn = table_name, nf = new_field, ft = field_type))
+c.execute('CREATE TABLE {tn} ({nf} {ft})'.format(tn = table_name1, nf = new_field, ft = field_type))
+c.execute('CREATE TABLE {tn} ({nf} {ft})'.format(tn = table_name2, nf = new_field, ft = field_type))
+c.execute('CREATE TABLE {tn} ({nf} {ft})'.format(tn = table_name3, nf = new_field, ft = field_type))
 
 conn.commit()
 conn.close()
--- update_db.py
+++ update_db.py
@@ -10,6 +10,10 @@ import sqlite3
 
 sqlite_file = 'course_listings.sqlite'   
 table_name = 'courses'
+table_name1 = 'prerequisites'  # name of the table to be created
+table_name2 = 'conflicts'  # name of the table to be created
+table_name3 = 'corequisites'  # name of the table to be created
+
 id_column = 'CourseNumber' # name of the PRIMARY KEY column
 new_column0 = 'CourseURL'
 new_column1 = 'CourseName'  # name of the new column
@@ -17,25 +21,9 @@ new_column2 = 'CourseDescription'  # name of the new column
 new_column3 = 'Department'
 new_column4 = 'Faculty'
 
-
-new_column5 = 'PreReq1'
-new_column6 = 'PreReq2'
-new_column7 = 'PreReq3'
-new_column8 = 'PreReq4'
-new_column9 = 'PreReq5'
-
-new_column10 = 'CoReq1'
-new_column11 = 'CoReq2'
-new_column12 = 'CoReq3'
-new_column13 = 'CoReq4'
-new_column14 = 'CoReq5'
-
-new_column15 = 'Conflict1'
-new_column16 = 'Conflict2'
-new_column17 = 'Conflict3'
-new_column18 = 'Conflict4'
-new_column19 = 'Conflict5'
-
+new_column5 = 'Prerequisite'
+new_column6 = 'Corequisite'
+new_column7 = 'Conflict'
 
 
 column_type = 'TEXT' # E.g., INTEGER, TEXT, NULL, REAL, BLOB
@@ -54,36 +42,15 @@ c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
         .format(tn=table_name, cn=new_column3, ct=column_type))
 c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
         .format(tn=table_name, cn=new_column4, ct=column_type))
+
 c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
-        .format(tn=table_name, cn=new_column5, ct=column_type))
+        .format(tn=table_name1, cn=new_column5, ct=column_type))
+
 c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
-        .format(tn=table_name, cn=new_column6, ct=column_type))
+        .format(tn=table_name2, cn=new_column7, ct=column_type))
+
 c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
-        .format(tn=table_name, cn=new_column7, ct=column_type))
-c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
-        .format(tn=table_name, cn=new_column8, ct=column_type))
-c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
-        .format(tn=table_name, cn=new_column9, ct=column_type))
-c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
-        .format(tn=table_name, cn=new_column10, ct=column_type))
-c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
-        .format(tn=table_name, cn=new_column11, ct=column_type))
-c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
-        .format(tn=table_name, cn=new_column12, ct=column_type))
-c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
-        .format(tn=table_name, cn=new_column13, ct=column_type))
-c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
-        .format(tn=table_name, cn=new_column14, ct=column_type))
-c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
-        .format(tn=table_name, cn=new_column15, ct=column_type))
-c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
-        .format(tn=table_name, cn=new_column16, ct=column_type))
-c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
-        .format(tn=table_name, cn=new_column17, ct=column_type))
-c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
-        .format(tn=table_name, cn=new_column18, ct=column_type))
-c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
-        .format(tn=table_name, cn=new_column19, ct=column_type))
+        .format(tn=table_name3, cn=new_column6, ct=column_type))
 
 # Committing changes and closing the connection to the database file
 conn.commit()
