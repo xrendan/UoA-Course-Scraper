1d21298d80970d4320fef991e79df7a07a2e009e
Brendan Samek <xrendan@gmail.com>
2016-11-13 00:05:42 -0700

    All the edge cases, this needs to be refactored

--- dependencyFinder.py
+++ dependencyFinder.py
@@ -12,7 +12,14 @@ conn = sqlite3.connect("course_listings.sqlite")
 c = conn.cursor()
 counter = 0 
 
+
+            
+
+
 c.execute("SELECT CourseNum, CourseDescription from courses")
+
+
+
 #
 #numRows = list(c)[0][0]
 
@@ -20,7 +27,7 @@ c.execute("SELECT CourseNum, CourseDescription from courses")
     
 for row in c:
     if "prerequisite." in row[1]:
-        pass
+        counter += 1
     if "prerequisite:" in row[1]:
         str = row[1]
         str = str.split("prerequisite:", 1)[1]
@@ -95,6 +102,81 @@ for row in c:
             print()
             
             counter += 1
+    if "prerequisite" in row[1]:
+        str = row[1]
+        str = str.split("prerequisite", 1)[1]
+        str = str.split(".", 1)[0].strip()
+        str = str.replace(",", "")
+        str = str.split(";", 1)[0].strip()
+        
+        
+        new_str = str.split(" ")
+        
+        if len(new_str) <= 2:
+            str = str.replace(" ", "")
+            print(row[0])
+            print(str)
+            print()
+            
+            counter += 1
+            
+            
+    if "Prerequisite" in row[1]:
+        str = row[1]
+        str = str.split("Prerequisite", 1)[1]
+        str = str.split(".", 1)[0].strip()
+        str = str.replace(",", "")
+        str = str.split(";", 1)[0].strip()
+        
+        
+        new_str = str.split(" ")
+        
+        if len(new_str) <= 2:
+            str = str.replace(" ", "")
+            print(row[0])
+            print(str)
+            print()
+            
+            counter += 1
+    
+            
+    if "prerequisites" in row[1]:
+        str = row[1]
+        str = str.split("prerequisites", 1)[1]
+        str = str.split(".", 1)[0].strip()
+        str = str.replace(",", "")
+        str = str.split(";", 1)[0].strip()
+        
+        
+        new_str = str.split(" ")
+        
+        if len(new_str) <= 2:
+            str = str.replace(" ", "")
+            print(row[0])
+            print(str)
+            print()
+            
+            counter += 1
+            
+            
+    if "Prerequisites" in row[1]:
+        str = row[1]
+        str = str.split("Prerequisites", 1)[1]
+        str = str.split(".", 1)[0].strip()
+        str = str.replace(",", "")
+        str = str.split(";", 1)[0].strip()
+        
+        
+        new_str = str.split(" ")
+        
+        if len(new_str) <= 2:
+            str = str.replace(" ", "")
+            print(row[0])
+            print(str)
+            print()
+            
+            counter += 1
+        
         
         
  
