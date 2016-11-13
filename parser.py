5d88e435a6bd70c4955f3bc37a2e2b406d967552
Brendan Samek <xrendan@gmail.com>
2016-11-12 18:07:40 -0700

    Made db creation less sketchy

--- parser.py
+++ parser.py
@@ -8,25 +8,6 @@ Created on Sat Nov 12 12:21:30 2016
 
 from bs4 import BeautifulSoup
 import requests
-import sqlite3
-
-   
-sqlite_file = 'course_listings.sqlite'    
-table_name = 'courses'  # name of the table to be created
-new_field = 'Course Number' # name of the column
-field_type = 'TEXT'  # column data type
-
-conn = sqlite3.connect(sqlite_file)
-c = conn.cursor()
-
-c.execute('CREATE TABLE {tn} ({nf} {ft})'.format(tn = table_name, nf = new_field, ft = field_type))
-
-conn.commit()
-conn.close()
-
-
-
-
 
 
 
---
+++ update_db.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 12 17:59:36 2016
+
+@author: brendan
+"""
+
+import sqlite3
+
+sqlite_file = 'course_listings.sqlite'   
+table_name = 'courses'
+id_column = 'Course Number' # name of the PRIMARY KEY column
+new_column1 = 'Course Name'  # name of the new column
+new_column2 = 'Course Description'  # name of the new column
+new_column3 = 'Department'
+new_column4 = 'Facul;ty'
+new_column5 = 'PreReq1'
+new_column6 = 'PreReq2'
+new_column7 = 'PreReq3'
+new_column8 = 'PreReq4'
+new_column9 = 'PreReq5'
+
+new_column10 = 'CoReq1'
+new_column11 = 'CoReq2'
+new_column12 = 'CoReq3'
+new_column13 = 'CoReq4'
+new_column14 = 'CoReq5'
+
+column_type = 'TEXT' # E.g., INTEGER, TEXT, NULL, REAL, BLOB
+
+# Connecting to the database file
+conn = sqlite3.connect(sqlite_file)
+c = conn.cursor()
+
+# A) Adding a new column without a row value
+c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
+        .format(tn=table_name, cn=new_column1, ct=column_type))
+c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
+        .format(tn=table_name, cn=new_column2, ct=column_type))
+c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
+        .format(tn=table_name, cn=new_column3, ct=column_type))
+c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
+        .format(tn=table_name, cn=new_column4, ct=column_type))
+c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
+        .format(tn=table_name, cn=new_column5, ct=column_type))
+c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
+        .format(tn=table_name, cn=new_column6, ct=column_type))
+c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
+        .format(tn=table_name, cn=new_column7, ct=column_type))
+c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
+        .format(tn=table_name, cn=new_column8, ct=column_type))
+c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
+        .format(tn=table_name, cn=new_column9, ct=column_type))
+c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
+        .format(tn=table_name, cn=new_column10, ct=column_type))
+c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
+        .format(tn=table_name, cn=new_column11, ct=column_type))
+c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
+        .format(tn=table_name, cn=new_column12, ct=column_type))
+c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
+        .format(tn=table_name, cn=new_column13, ct=column_type))
+c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
+        .format(tn=table_name, cn=new_column14, ct=column_type))
+
+
+# Committing changes and closing the connection to the database file
+conn.commit()
+conn.close()
